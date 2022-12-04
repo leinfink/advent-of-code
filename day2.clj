bc576ee6d1b76b87f91bdaa2e4723d7808c1989c
Henrik Hörmann <leinfink@mailbox.org>
2022-12-04 14:07:08 +0100

    day 2 puzzle 1

---
+++ day2.clj
@@ -0,0 +1,22 @@
+(require '[clojure.string :as str])
+
+(defn read-strategy [input]
+  (map #(str/split % #" ")
+       (str/split-lines input)))
+
+(defn round-score [[enemy, myself]]
+  (let [enemy-points ({"A" 1, "B" 2, "C" 3} enemy)
+        my-points  ({"X" 1, "Y" 2, "Z" 3} myself)]
+    (cond
+      (= enemy-points my-points) (+ 3 my-points)
+      (= my-points (case enemy-points
+                     1 2
+                     2 3
+                     3 1)) (+ 6 my-points)
+      :else my-points)))
+
+(defn get-scores [strategy]
+  (reduce (fn [score next-round]
+            (+ score (round-score next-round)))
+          0
+          strategy))
