384cf8f2b5af2a139b802407a5c579dca8b151dd
Henrik Hörmann <leinfink@mailbox.org>
2022-12-05 14:15:04 +0100

    reworked folder structure

---
+++ test/aoc22/day1-test.clj
@@ -0,0 +1,11 @@
+(ns aoc22.day1-test
+  (:require
+   [clojure.test :refer [deftest is]]
+   [aoc22.day1 :as day1]))
+
+(defn read-input [day]
+  (slurp (str "inputs/day" day)))
+
+(deftest day1-test
+  (is (= (day1/part1 (read-input 1))
+         70720)))
