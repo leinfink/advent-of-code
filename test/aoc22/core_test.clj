a779943ade440be63bca557a22d36bea252cfccd
Henrik Hörmann <leinfink@mailbox.org>
2022-12-05 19:42:01 +0100

    fixed automated testing

---
+++ test/aoc22/core_test.clj
@@ -0,0 +1,41 @@
+(ns aoc22.core-test
+  (:require
+   [clojure.string :as str]
+   [clojure.test :refer [deftest testing is]]))
+
+(defn- read-file [day type]
+  (slurp (str "inputs/aoc22/day" day "." (name type))))
+
+(defn- parse-answer [input]
+  (str/split-lines input))
+
+(defn read-puzzle
+  "Returns a hashmap with the input and the answers for the day."
+  [day]
+  (into {}
+        (for [[type modifier] {:input identity :answer parse-answer}]
+          [type (modifier (read-file day type))])))
+
+(defn- ns-concat [namespace]
+  (fn [& x] (symbol (apply str (conj x namespace)))))
+
+(defmacro def-daytest [day]
+  (let [ns-day (ns-concat (str "aoc22.day" day))]
+    `(do (require '~(ns-day))
+         (deftest ~(ns-day "-test")
+           (let [puzzle# (read-puzzle ~day)
+                 input# (:input puzzle#)
+                 answer# (:answer puzzle#)]
+             (testing "Part 1"
+               (is (= (first answer#)
+                      (str (~(ns-day "/part1") input#)))))
+             (testing "Part 2"
+               (is (= (second answer#)
+                      (str (~(ns-day "/part2") input#))))))))))
+
+(defmacro generate-daytests [max-day]
+  `(do
+     ~@(for [day (range 1 (inc max-day))]
+         `(def-daytest ~day))))
+
+(generate-daytests 4)
