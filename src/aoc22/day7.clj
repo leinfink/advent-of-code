d487f50026200028fc3bbff07e45bc1f616f864a
Henrik Hörmann <leinfink@mailbox.org>
2022-12-10 21:11:37 +0100

    solved part 2 day 7

--- src/aoc22/day7.clj
+++ src/aoc22/day7.clj
@@ -21,9 +21,18 @@
         size (+ (reduce + (map first subcounts))
                 (reduce + (map (fn [[f _]]
                                  (if (= f "dir") 0 (parse-long f))) files)))]
-    [size, (if (< size 100000) (conj chosen size) chosen)]))
+    [size, (conj chosen [pos size])]))
 
-(defn solve [s]
+(defn part1 [s]
   (let [[dirs _]  (reduce build-tree [{:/ {}}, [:/]] (parse s))
         [_ chosen-dirs]  (count-dirs [:/ dirs])]
-    (reduce + chosen-dirs)))
+    (prn chosen-dirs)
+    (reduce + (filter #(<= % 100000) (map second chosen-dirs)))))
+
+
+(defn part2 [s]
+  (let [[dirs _]  (reduce build-tree [{:/ {}}, [:/]] (parse s))
+        [total-size chosen-dirs]  (count-dirs [:/ dirs])
+        needed  (- 30000000 (- 70000000 total-size))]
+    (first (sort < (filter #(>= % needed) (map second chosen-dirs))))
+    ))
