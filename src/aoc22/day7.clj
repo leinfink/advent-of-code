39ada410ef4aa99cadb872ddfa97c504af0d3298
Henrik Hörmann <leinfink@mailbox.org>
2022-12-10 21:14:14 +0100

    tiny refactor

--- src/aoc22/day7.clj
+++ src/aoc22/day7.clj
@@ -21,18 +21,16 @@
         size (+ (reduce + (map first subcounts))
                 (reduce + (map (fn [[f _]]
                                  (if (= f "dir") 0 (parse-long f))) files)))]
-    [size, (conj chosen [pos size])]))
+    [size, (conj chosen size)]))
 
 (defn part1 [s]
   (let [[dirs _]  (reduce build-tree [{:/ {}}, [:/]] (parse s))
         [_ chosen-dirs]  (count-dirs [:/ dirs])]
-    (prn chosen-dirs)
-    (reduce + (filter #(<= % 100000) (map second chosen-dirs)))))
+    (reduce + (filter #(<= % 100000) chosen-dirs))))
 
 
 (defn part2 [s]
   (let [[dirs _]  (reduce build-tree [{:/ {}}, [:/]] (parse s))
         [total-size chosen-dirs]  (count-dirs [:/ dirs])
         needed  (- 30000000 (- 70000000 total-size))]
-    (first (sort < (filter #(>= % needed) (map second chosen-dirs))))
-    ))
+    (first (sort < (filter #(>= % needed)  chosen-dirs)))))
