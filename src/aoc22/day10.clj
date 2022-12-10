64f241eb5c95f2af05e080f2ace921241090bbc7
Henrik Hörmann <leinfink@mailbox.org>
2022-12-10 16:51:33 +0100

    refactor day10

--- src/aoc22/day10.clj
+++ src/aoc22/day10.clj
@@ -1,4 +1,5 @@
-(ns aoc22.day10)
+(ns aoc22.day10
+  (:require [clojure.string :as str]))
 
 (defn parse [s]
   (for [[_ cmd val] (re-seq #"(\w+)(?: (\-?\d+))?" s)]
@@ -7,15 +8,24 @@
 (def cmd-times {:noop 1, :addx 2})
 (def cmd-func {:noop (fn [x _] x), :addx +})
 
-(defn run [[cycle state] [cmd val]]
-  [(+ cycle (cmd cmd-times)), ((cmd cmd-func) state val)])
+(defn run [[cycl state] [cmd val]]
+  [(+ cycl (cmd cmd-times)), ((cmd cmd-func) state val)])
 
 (defn exec [s]
   (apply hash-map (flatten (reductions run [0 1] (parse s)))))
 
-(defn signal [vals n]
-    (* n (some vals (range (dec n) 0 -1))))
+(defn register [regs cycl]
+  (some regs (range (dec cycl) -1 -1)))
 
 (defn part1 [s]
-  (let [res (exec s)]
-    (reduce + (map (partial signal res) [20 60 100 140 180 220]))))
+  (reduce + (map #(* % (register (exec s) %)) [20 60 100 140 180 220])))
+
+(defn part2 [s]
+  (let [regs (exec s)]
+    (->> (for [row (range 0 6)]
+           (->> (for [px (range 0 40)
+                      :let [cycl (inc (+ px (* row 40)))
+                            dist (abs (- (register regs cycl) px))]]
+                  (if (< dist 2) "#" "."))
+                str/join))
+         (str/join "\n"))))
