cca379f65fc5145a480f4d048b7cfc297914dbe6
Henrik Hörmann <leinfink@mailbox.org>
2022-12-11 13:10:22 +0100

    a bit of golfing

--- src/aoc22/day10.clj
+++ src/aoc22/day10.clj
@@ -5,27 +5,23 @@
   (for [[_ cmd val] (re-seq #"(\w+)(?: (\-?\d+))?" s)]
     [(keyword cmd) (parse-long (or val ""))]))
 
-(def cmd-times {:noop 1, :addx 2})
-(def cmd-func {:noop (fn [x _] x), :addx +})
-
 (defn run [[cycl state] [cmd val]]
-  [(+ cycl (cmd cmd-times)), ((cmd cmd-func) state val)])
+  [(+ cycl (cmd {:noop 1, :addx 2})), ; cycle-number
+   ((cmd {:noop (fn [x _] x), :addx +}) state val)]) ; updated state
 
-(defn exec [s]
-  (apply hash-map (flatten (reductions run [0 1] (parse s)))))
+(defn exec [s] (into {} (reductions run [0 1] (parse s))))
 
-(defn register [regs cycl]
-  (some regs (range (dec cycl) -1 -1)))
+(defn get-reg [regs cycl] (some regs (range (dec cycl) -1 -1)))
 
 (defn part1 [s]
-  (reduce + (map #(* % (register (exec s) %)) [20 60 100 140 180 220])))
+  (reduce + (map #(* % (get-reg (exec s) %)) [20 60 100 140 180 220])))
 
 (defn part2 [s]
-  (let [regs (exec s)]
-    (->> (for [row (range 0 6)]
-           (->> (for [px (range 0 40)
-                      :let [cycl (inc (+ px (* row 40)))
-                            dist (abs (- (register regs cycl) px))]]
-                  (if (< dist 2) "#" "."))
-                str/join))
+  (let [regs (exec s), width 40]
+    (->> (for [row (range 0 6), px (range 0 width)
+               :let [cycl (inc (+ px (* row width)))
+                     dist (abs (- (get-reg regs cycl) px))]]
+           (if (< dist 2) "#" "."))
+         (partition width)
+         (map str/join)
          (str/join "\n"))))
