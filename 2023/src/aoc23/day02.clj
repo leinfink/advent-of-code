35817db81106ca928d0ddbfa00dbd9ed35644c2f
Henrik Hörmann <leinfink@mailbox.org>
2023-12-02 10:14:28 +0100

    2023 day 2 part 2 submitted

--- 2023/src/aoc23/day02.clj
+++ 2023/src/aoc23/day02.clj
@@ -1,9 +1,5 @@
-(ns aoc22.day02
+(ns aoc23.day02
   (:require [clojure.string :as str]))
-
-(defn parse [input]
-  (->> (str/split-lines input)
-       (map prepare-line)))
 
 (defn prepare-line [line]
   (let [[id reveals] (str/split line #":")]
@@ -12,7 +8,7 @@
      (for [rev (str/split reveals #";")
            :let [balls (str/split (str/trim rev) #",? ")
                  [counts colors] (apply map list (partition 2 balls))]]
-           (zipmap colors (map parse-long counts)))}))
+       (zipmap colors (map parse-long counts)))}))
 
 (defn possible-reveal? [config reveal]
   (every? (fn [[color count]] (>= (get config color 0) count)) reveal))
@@ -20,9 +16,19 @@
 (defn possible-config? [config reveals]
   (every? #(possible-reveal? config %) reveals))
 
+(defn multiply-minimum-config [reveals]
+  (reduce * (vals (apply merge-with max reveals))))
+
 (defn solve [input config]
   (->> (str/split-lines input)
        (map prepare-line)
        (filter #(possible-config? config (:reveals %)))
        (map :id)
+       (reduce +)))
+
+(defn solve2 [input]
+  (->> (str/split-lines input)
+       (map prepare-line)
+       (map :reveals)
+       (map multiply-minimum-config)
        (reduce +)))
