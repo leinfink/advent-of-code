286289fb07d8f5dcef1cf2b8b9ce86a9f5f108b7
Henrik Hörmann <leinfink@mailbox.org>
2023-12-04 14:59:58 +0100

    day04 part 2 submitted

--- 2023/src/aoc23/day04.clj
+++ 2023/src/aoc23/day04.clj
@@ -15,8 +15,32 @@
        (reduce (fn [v _] (* v 2)) 1/2)
        int))
 
+(defn count-line [line]
+  {:id (:id line) :count (->> (set (:mine line))
+                              (clojure.set/intersection (set (:winners line)))
+                              count)})
+
 (defn part1 [input]
   (->> (str/split-lines input)
        (map prepare-line)
        (map calc-line)
        (reduce +)))
+
+(defn add-cards [cards card]
+  (apply conj cards
+        (for [i (range (:count card))]
+          (some (fn [x] (when (= (:id x) (+ (:id card) (inc i))) x)) cards))))
+
+(defn get-cards [cards coll]
+  (if (empty? cards) coll
+      (recur (add-cards (rest cards) (first cards))
+             (conj coll (first cards)))))
+    
+
+(defn cards [input]
+  (count 
+  (get-cards  (->> (str/split-lines input)
+                   (map prepare-line)
+                   (map count-line))
+              '())))
+
