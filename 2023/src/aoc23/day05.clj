a7abe2854e2a27eea2e4f85f3de3733a7ca872b1
Henrik Hörmann <leinfink@mailbox.org>
2023-12-06 01:12:38 +0100

    day 5 part 2 submitted

--- 2023/src/aoc23/day05.clj
+++ 2023/src/aoc23/day05.clj
@@ -25,12 +25,19 @@
      :humidity-to-location (parse-map (inputs 7))}))
 
 
-(defn map-fn [m source]
-  (or (some (fn [[dest-start source-start length]]
-              (when (<= source-start source (+ (dec length) source-start))
-                (+ dest-start (- source source-start))))
-            m)
-      source))
+(defn map-fn
+  ([backwards m dest]
+   (or (some (fn [[dest-start source-start length]]
+               (when (<= dest-start dest (+ (dec length) dest-start))
+                 (+ source-start (- dest dest-start))))
+             m)
+       dest))
+  ([m source]
+   (or (some (fn [[dest-start source-start length]]
+               (when (<= source-start source (+ (dec length) source-start))
+                 (+ dest-start (- source source-start))))
+             m)
+       source)))
 
 (defn seed-to-loc [seed maps]
   (->> (map-fn (:seed-to-soil maps) seed)
@@ -42,6 +49,50 @@
        (map-fn (:humidity-to-location maps))
        ))
 
+(defn loc-to-seed [loc maps]
+  (->>
+   (map-fn true (:humidity-to-location maps) loc)
+   (map-fn true (:temperature-to-humidity maps))
+   (map-fn true (:light-to-temperature maps))
+   (map-fn true (:water-to-light maps))
+   (map-fn true  (:fertilizer-to-water maps))
+   (map-fn true (:soil-to-fertilizer maps))
+   (map-fn true(:seed-to-soil maps)))
+
+  )
+  
 (defn part1 [input]
   (let [input (parse input)]
     (first (sort (map #(seed-to-loc % input) (:seeds input))))))
+
+(defn possible-locations [loc-map]
+  (flatten (map (fn [[dest source r]]
+                     (for [i (range dest (+ dest r))]
+                       i)) loc-map)))
+
+(defn is-in-seeds [n seeds]
+  (let [pairs (partition 2 seeds)]
+    (some #(when (<= (first %) n (+ (first %) (dec (second %))))
+             (first %)) pairs)))
+
+#_(apply max
+         (map
+          #(+ (first %) (dec (second %)))
+          (partition 2 (:seeds (parse input)))))
+
+;; 3905942342
+
+(defn part2 [input]
+  (let [input (parse input)]
+    (some (fn [x]
+            (when (is-in-seeds (loc-to-seed x input) (:seeds input))
+                   x))
+          (range))))
+
+(loc-to-seed 1 (parse input))
+
+;; (part2 input)
+
+;; (loc-to-seed 0 (parse input))
+;; (is-in-seeds 1693708620 (:seeds (parse input)))
+
