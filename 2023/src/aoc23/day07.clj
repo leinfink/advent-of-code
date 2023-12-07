25569b719cb5a7d879c6dc635876b86b95c2a40f
Henrik Hörmann <leinfink@mailbox.org>
2023-12-07 19:53:45 +0100

    aoc23 day 7 refactor

--- 2023/src/aoc23/day07.clj
+++ 2023/src/aoc23/day07.clj
@@ -1,8 +1,3 @@
-(comment
-  (def input (slurp "inputs/input7.txt"))
-  (def input (slurp "inputs/example7.txt"))
-  )
-
 (ns aoc23.day07
   (:require [clojure.string :as str]))
 
@@ -11,64 +6,49 @@
        (map #(str/split % #" +"))
        (map (fn [x] {:hand (x 0) :bid (parse-long (x 1))}))))
 
-(def cards (reverse "AKQJT98765432"))
+(def cards1 (reverse "AKQJT98765432"))
 (def cards2 (reverse "AKQT98765432J"))
-
 (def types (reverse [:five :four :full-house :three :two-pair :one-pair :high-card]))
 
 (defn compare-type [type-a type-b]
   (compare (.indexOf types type-a) (.indexOf types type-b)))
 
-(defn compare-highest-cards [a b]
+(defn compare-highest-cards [a b cards]
   (let [comparison (compare (str/index-of cards (first a))
                             (str/index-of cards (first b)))]
     (if (= 0 comparison)
-      (compare-highest-cards (rest a) (rest b))
+      (compare-highest-cards (rest a) (rest b) cards)
       comparison)))
 
-(defn compare-highest-cards-2 [a b]
-  (let [comparison (compare (str/index-of cards2 (first a))
-                            (str/index-of cards2 (first b)))]
-    (if (= 0 comparison)
-      (compare-highest-cards-2 (rest a) (rest b))
-      comparison)))
-
-
-(defn hand-type [hand]
-  (let [freqs (frequencies (vals (frequencies hand)))]
-    (cond
+(defn calc-hand-type [freqs]
+  (cond
       (freqs 5) :five
       (freqs 4) :four
       (and (freqs 3) (freqs 2)) :full-house
       (freqs 3) :three
       (= 2 (freqs 2)) :two-pair
       (freqs 2) :one-pair
-      :else :high-card)))
+      :else :high-card))
+
+(defn hand-type [hand]
+  (calc-hand-type (frequencies (vals (frequencies hand)))))
 
 (defn hand-type2 [hand]
   (let [joker-count (or ((frequencies hand) \J) 0)
         v (sort > (vals (frequencies (str/replace hand "J" ""))))
-        joker-update (conj (rest v) (+ joker-count (or (first v) 0)))
-        freqs (frequencies joker-update)]
-    (cond
-      (freqs 5) :five
-      (freqs 4) :four
-      (and (freqs 3) (freqs 2)) :full-house
-      (freqs 3) :three
-      (= 2 (freqs 2)) :two-pair
-      (freqs 2) :one-pair
-      :else :high-card)))
+        joker-update (conj (rest v) (+ joker-count (or (first v) 0)))]
+    (calc-hand-type (frequencies joker-update))))
 
 (defn compare-hands [a b]
   (let [type-comparison (compare-type (hand-type a) (hand-type b))]
     (if (= 0 type-comparison)
-      (compare-highest-cards a b)
+      (compare-highest-cards a b cards1)
       type-comparison)))
 
 (defn compare-hands-2 [a b]
   (let [type-comparison (compare-type (hand-type2 a) (hand-type2 b))]
     (if (= 0 type-comparison)
-      (compare-highest-cards-2 a b)
+      (compare-highest-cards a b cards2)
       type-comparison)))
 
 (defn part1 [input]
@@ -82,9 +62,3 @@
        (sort #(compare-hands-2 (:hand %1) (:hand %2)))
        (map-indexed (fn [rank item] (* (inc rank) (:bid item))))
        (reduce +)))
-
-(hand-type2 "JJJJJ")
-(compare-hands-2 )
-
-(part1 input)
-(part2 input)
