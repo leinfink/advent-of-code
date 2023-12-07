ee9ba66d8975aadbe55db313b462b32a7ff4f52b
Henrik Hörmann <leinfink@mailbox.org>
2023-12-07 21:05:57 +0100

    aoc 23 day 07 refactor

--- 2023/src/aoc23/day07.clj
+++ 2023/src/aoc23/day07.clj
@@ -6,8 +6,8 @@
        (map #(str/split % #" +"))
        (map (fn [x] {:hand (x 0) :bid (parse-long (x 1))}))))
 
-(def cards1 (reverse "AKQJT98765432"))
-(def cards2 (reverse "AKQT98765432J"))
+(def ^:dynamic cards (reverse "AKQJT98765432"))
+
 (def types (reverse [:five :four :full-house :three :two-pair :one-pair :high-card]))
 
 (defn compare-type [type-a type-b]
@@ -30,25 +30,13 @@
       (freqs 2) :one-pair
       :else :high-card))
 
-(defn hand-type [hand]
+(defn ^:dynamic hand-type [hand]
   (calc-hand-type (frequencies (vals (frequencies hand)))))
-
-(defn hand-type2 [hand]
-  (let [joker-count (or ((frequencies hand) \J) 0)
-        v (sort > (vals (frequencies (str/replace hand "J" ""))))
-        joker-update (conj (rest v) (+ joker-count (or (first v) 0)))]
-    (calc-hand-type (frequencies joker-update))))
 
 (defn compare-hands [a b]
   (let [type-comparison (compare-type (hand-type a) (hand-type b))]
     (if (= 0 type-comparison)
-      (compare-highest-cards a b cards1)
-      type-comparison)))
-
-(defn compare-hands-2 [a b]
-  (let [type-comparison (compare-type (hand-type2 a) (hand-type2 b))]
-    (if (= 0 type-comparison)
-      (compare-highest-cards a b cards2)
+      (compare-highest-cards a b cards)
       type-comparison)))
 
 (defn part1 [input]
@@ -57,8 +45,15 @@
        (map-indexed (fn [rank item] (* (inc rank) (:bid item))))
        (reduce +)))
 
+(def cards2 (reverse "AKQT98765432J"))
+
+(defn hand-type2 [hand]
+  (let [joker-count (or ((frequencies hand) \J) 0)
+        v (sort > (vals (frequencies (str/replace hand "J" ""))))
+        joker-update (conj (rest v) (+ joker-count (or (first v) 0)))]
+    (calc-hand-type (frequencies joker-update))))
+
 (defn part2 [input]
-  (->> (parse input)
-       (sort #(compare-hands-2 (:hand %1) (:hand %2)))
-       (map-indexed (fn [rank item] (* (inc rank) (:bid item))))
-       (reduce +)))
+  (binding [hand-type hand-type2
+            cards cards2]
+    (part1 input)))
