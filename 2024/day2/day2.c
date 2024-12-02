f59ac66a9fe4edffdf0d54b4c5ab3a3600e5587f
Henrik Hörmann <leinfink@mailbox.org>
2024-12-03 00:24:52 +0100

    Refactor 24 day2 C

--- 2024/day2/day2.c
+++ 2024/day2/day2.c
@@ -3,38 +3,21 @@
 #include <string.h>
 
 #define FILENAME "input.txt"
+#define LINEMAX 10124
 
 int check_record(char *, int *);
 
 int main(void) {
   FILE *fp;
-  fp = fopen(FILENAME, "r");
-  char line[1024];
-  char *num;
-  int last, cur, is_safe, dt;
-  int dir, cur_dir; /* 0: no direction yet, 1: increasing, -1: decreasing */
+  char line[LINEMAX];
   int safe_records = 0;
 
   /* part 1 */
 
+  fp = fopen(FILENAME, "r");
+
   while (fgets(line, sizeof line, fp) != NULL) {
-    last = atoi(strtok(line, " "));
-    num = strtok(NULL, " ");
-    is_safe = 1, dir = 0;
-    while (num != NULL) {
-      cur = atoi(num);
-      dt = abs(cur - last);
-      cur_dir = (cur > last) ? 1 : -1; /* if cur == last, then dt is 0 anyway */
-      if (dt > 0 && dt <= 3 && (dir == 0 || dir * cur_dir == 1)) {
-        dir = cur_dir;
-      } else {
-        is_safe = 0;
-        break;
-      }
-      last = cur;
-      num = strtok(NULL, " ");
-    }
-    safe_records += is_safe;
+    safe_records += check_record(line, NULL);
   }
 
   fclose(fp);
@@ -43,10 +26,9 @@ int main(void) {
 
   /* part 2 */
 
-  char linecp[1024];
-  char newrec[1024];
-  char newreccp[1024];
-  int numlen, slen, safe, k, m, len;
+  char linecp[LINEMAX], newrec[LINEMAX];
+  int numcount, slen, safe, k, m, len;
+
   safe_records = 0;
 
   fp = fopen(FILENAME, "r");
@@ -54,9 +36,9 @@ int main(void) {
   while (fgets(line, sizeof line, fp) != NULL) {
     strcpy(linecp, line);
     slen = strlen(linecp);
-    safe = check_record(line, &numlen);
+    safe = check_record(line, &numcount);
     if (!safe) {
-      for (int i = 0; i < numlen; ++i) {
+      for (int i = 0; i < numcount; ++i) {
         k = 0, m = 0;
         for (int j = 0; j < slen; ++j) {
           if (linecp[j] == ' ') {
@@ -73,38 +55,45 @@ int main(void) {
           } else {
             ++m;
           }
-      }
-      newrec[slen-m] = '\0';
-      strcpy(newreccp, newrec);
-      safe = check_record(newrec, &len);
-      if (safe) {
-        break;
+        }
+        newrec[slen - m] = '\0';
+        safe = check_record(newrec, NULL);
+        if (safe) {
+          break;
+        }
       }
     }
+    safe_records += safe;
   }
-  safe_records += safe;
+
+  printf("Result part 2: %d\n", safe_records);
+
+  fclose(fp);
+
+  return EXIT_SUCCESS;
 }
 
-printf("Result part 2: %d\n", safe_records);
-
-fclose(fp);
-
-return EXIT_SUCCESS;
-}
-
-int check_record(char *record, int *len) {
+int check_record(char *record, int *numcount) {
   char *num;
-  int last, cur, dt, is_safe, dir, cur_dir;
-  /* printf("--- checking %s\n", record); */
+  int last, cur, is_safe, dt;
+  int dir, cur_dir; /* 0: no direction yet, 1: increasing, -1: decreasing */
+
   last = atoi(strtok(record, " "));
   num = strtok(NULL, " ");
-  *len = 1;
-  is_safe = 1, dir = 0;
+
+  if (numcount != NULL) {
+    *numcount = 1;
+  }
+
+  is_safe = 1;
+  dir = 0;
+
   while (num != NULL) {
-    ++*len;
+    if (numcount != NULL) {
+      ++*numcount;
+    }
     cur = atoi(num);
     dt = abs(cur - last);
-    /* printf("abs of %d\n", dt); */
     cur_dir = (cur > last) ? 1 : -1; /* if cur == last, then dt is 0 anyway */
     if (dt > 0 && dt <= 3 && (dir == 0 || dir * cur_dir == 1)) {
       dir = cur_dir;
@@ -115,7 +104,6 @@ int check_record(char *record, int *len) {
     last = cur;
     num = strtok(NULL, " ");
   }
-  /* printf("TRUTH: %d OHA\n", is_safe); */
 
   return is_safe;
 }
