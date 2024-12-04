2b9dff0c9733dcee03222758bbd555d30f4ba73b
Henrik Hörmann <leinfink@mailbox.org>
2024-12-04 10:48:50 +0100

    Refactor 24 day 3 C

--- 2024/day3/day3.c
+++ 2024/day3/day3.c
@@ -1,20 +1,22 @@
 #include <regex.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define FILENAME "input.txt"
 #define MAXLINE 1024 * 10
+#define MATCH_GROUPS 3
+
+int solve(bool);
+
+FILE *fp;
+regex_t regex;
+regmatch_t pmatch[MATCH_GROUPS];
+int operands[MATCH_GROUPS - 1];
 
 int main(void) {
-  FILE *fp;
-  char line[MAXLINE];
-  regex_t regex;
-  int match_groups = 3;
-  regmatch_t pmatch[match_groups];
-  int operands[match_groups - 1];
-  unsigned int result = 0;
-  int pos = 0;
+  int result;
 
   if (regcomp(&regex, "mul\\(([1-9][0-9]*),([1-9][0-9]*)\\)", REG_EXTENDED)) {
     return EXIT_FAILURE;
@@ -22,78 +24,67 @@ int main(void) {
 
   fp = fopen(FILENAME, "r");
 
-  while (!feof(fp)) {
-    if (fgets(line, sizeof(line), fp) != NULL) {
-      pos = 0;
-      while (regexec(&regex, line + pos, match_groups, pmatch, 0) == 0) {
-        int matches = regex.re_nsub;
-        for (int i = 1; i <= matches; ++i) {
-          char *p = line + pos + pmatch[i].rm_so;
-          operands[i - 1] = atoi(p);
-        }
-        int j = 1;
-        for (int i = 0; i < match_groups - 1; ++i) {
-          j *= operands[i];
-        }
-        result += j;
-        pos += pmatch[matches].rm_eo;
-      }
-    }
-  }
-  regfree(&regex);
-  fclose(fp);
+  result = solve(false);
   printf("Result part 1: %d\n", result);
 
-  int active = 1;
-  result = 0;
+  result = solve(true);
+  printf("Result part 2: %d\n", result);
 
-  if (regcomp(&regex, "mul\\(([1-9][0-9]*),([1-9][0-9]*)\\)", REG_EXTENDED)) {
-    return EXIT_FAILURE;
-  }
+  fclose(fp);
 
-  fp = fopen(FILENAME, "r");
+  return EXIT_SUCCESS;
+}
+
+int solve(bool check_dos) {
+  char line[MAXLINE];
+  int pos;
+  int result = 0;
+  bool active = true;
 
   while (!feof(fp)) {
     if (fgets(line, sizeof(line), fp) != NULL) {
       pos = 0;
-      while (regexec(&regex, line + pos, match_groups, pmatch, 0) == 0) {
-        char shortened[MAXLINE];
-        strncpy(shortened, line, pos + pmatch[2].rm_so);
-        shortened[pos + pmatch[2].rm_so] = '\0';
-        char *tmp = shortened;
-        char *last_do = tmp;
-        int count = 0;
-        while ((tmp = strstr(tmp, "do()")) != NULL) {
-          tmp++;
-          last_do = tmp;
-          count++;
+      while (regexec(&regex, line + pos, MATCH_GROUPS, pmatch, 0) == 0) {
+        if (check_dos) {
+          char shortened[MAXLINE];
+          strncpy(shortened, line, pos + pmatch[2].rm_so);
+          shortened[pos + pmatch[2].rm_so] = '\0';
+          char *tmp = shortened;
+          char *last_do = tmp;
+          int count = 0;
+          while ((tmp = strstr(tmp, "do()")) != NULL) {
+            ++tmp;
+            ++count;
+            last_do = tmp;
+          }
+          char *tmp2 = shortened;
+          char *last_dont = tmp2;
+          while ((tmp2 = strstr(tmp2, "don't()")) != NULL) {
+            ++tmp2;
+            ++count;
+            last_dont = tmp2;
+          }
+          active = count == 0 ? active : (strlen(last_do) < strlen(last_dont));
         }
-        char *tmp2 = shortened;
-        char *last_dont = tmp2;
-        while ((tmp2 = strstr(tmp2, "don't()")) != NULL) {
-          tmp2++;
-          last_dont = tmp2;
-          count++;
-        }
-        active = count == 0 ? active : (strlen(last_do) < strlen(last_dont));
-
+        
         int matches = regex.re_nsub;
-        for (int i = 1; i <= matches; ++i) {
-          char *p = line + pos + pmatch[i].rm_so;
-          operands[i - 1] = atoi(p);
-        }
-        int j = 1;
-        for (int i = 0; i < match_groups - 1; ++i) {
-          j *= operands[i];
-        }
+        
         if (active) {
+          for (int i = 1; i <= matches; ++i) {
+            char *p = line + pos + pmatch[i].rm_so;
+            operands[i - 1] = atoi(p);
+          }
+          int j = 1;
+          for (int i = 0; i < MATCH_GROUPS - 1; ++i) {
+            j *= operands[i];
+          }
+
           result += j;
         }
         pos += pmatch[matches].rm_eo;
       }
     }
   }
-  regfree(&regex);
-  fclose(fp);
-  printf("Result part 2: %d\n", result);
+  rewind(fp);
+  return result;
 }
