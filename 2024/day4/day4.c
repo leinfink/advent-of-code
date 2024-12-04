dd15044f6cd957afed11cf0c895accfeee85b9a3
Henrik Hörmann <leinfink@mailbox.org>
2024-12-04 14:53:22 +0100

    24 day 4 C part 2

--- 2024/day4/day4.c
+++ 2024/day4/day4.c
@@ -16,40 +16,94 @@ struct vec point;
 struct vec dir[DIR_LEN] = {{1, 1},  {1, 0},  {-1, 0}, {-1, -1},
                            {-1, 1}, {1, -1}, {0, 1},  {0, -1}};
 
-bool find_mas(struct vec, struct vec);
+int count_mas(struct vec);
+bool find_xmas(struct vec);
+bool find(char*, struct vec, struct vec);
 
 int main(void) {
+  int i = 0;
   fp = fopen(FILENAME, "r");
-
-  int i;
   while (fgets(grid[i], LENGTH + 2, fp) != NULL) {
     ++i;
   }
+  fclose(fp);
 
   if (i != LENGTH) {
     return EXIT_FAILURE;
   }
 
+  /* part 1 */
+
   int count = 0;
-  for (i = 0; i < LENGTH; ++i) {
+  for (int i = 0; i < LENGTH; ++i) {
     for (int j = 0; j < LENGTH; ++j) {
       point.x = i;
       point.y = j;
       if (grid[point.y][point.x] == 'X') {
-        for (int k = 0; k < DIR_LEN; ++k) {
-          count += find_mas(point, dir[k]);
-        }
+        count += count_mas(point);
       }
     }
   }
 
-  fclose(fp);
   printf("Result part 1: %d\n", count);
+
+  /* part 2 */
+
+  count = 0;
+  for (int i = 0; i < LENGTH; ++i) {
+    for (int j = 0; j < LENGTH; ++j) {
+      point.x = i;
+      point.y = j;
+      if (grid[point.y][point.x] == 'A') {
+        count += find_xmas(point);
+      }
+    }
+  }
+
+  printf("Result part 2: %d\n", count);
+
   return EXIT_SUCCESS;
 }
 
-bool find_mas(struct vec point, struct vec dir) {
+int count_mas(struct vec point) {
   char mas[4] = "MAS";
+  int count = 0;
+  for (int k = 0; k < DIR_LEN; ++k) {
+    count += find(mas, point, dir[k]);
+  }
+  return count;
+}
+
+bool find_xmas(struct vec point) {
+  char c;
+  c = grid[point.y - 1][point.x - 1];
+  if (c == 'S') {
+    if (grid[point.y + 1][point.x + 1] != 'M') {
+      return 0;
+    }
+  } else if (c == 'M') {
+    if (grid[point.y + 1][point.x + 1] != 'S') {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
+  c = grid[point.y + 1][point.x - 1];
+  if (c == 'S') {
+    if (grid[point.y - 1][point.x + 1] != 'M') {
+      return 0;
+    }
+  } else if (c == 'M') {
+    if (grid[point.y - 1][point.x + 1] != 'S') {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
+  return 1;
+}
+
+bool find(char* mas, struct vec point, struct vec dir) {
   for (int i = 1; i < 4; ++i) {
     char c = grid[point.y + dir.y * i][point.x + dir.x * i];
     if (mas[i - 1] != c) {
