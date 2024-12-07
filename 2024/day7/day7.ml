d63d48b9757683edac5fbce185851ff6bb31a509
Henrik Hörmann <leinfink@mailbox.org>
2024-12-07 15:57:21 +0100

    24 day7 part2 OCaml

--- 2024/day7/day7.ml
+++ 2024/day7/day7.ml
@@ -1,8 +1,16 @@
-type operator = Add | Multiply
-type expr = Operation of operation | Int of int
-and operation = {op : operator; lh : expr; rh: int} 
+(* Solve *)
 
-let calc = function | Add -> ( + ) | Multiply -> ( * )
+type operator = Add | Multiply | Concat
+type expr = Operation of operation | Int of int
+and operation = {op : operator; lh : expr; rh: int}
+
+let active_operators = ref []
+
+let calc = function
+  | Add -> ( + )
+  | Multiply -> ( * )
+  | Concat -> (fun a b -> int_of_string (String.cat (string_of_int a) (string_of_int b)))
+              
 let rec evaluate expr : int = match expr with
   | Operation {op; lh; rh} -> calc op (evaluate lh) rh
   | Int n -> n
@@ -11,7 +19,7 @@ let fill_eq (eq: int list) (ops: operator list) : expr =
   List.fold_left2 (fun acc e o -> Operation {op = o; lh = acc; rh = e})
     (Int 0) eq (Add :: ops)
 
-let prepend_option os = List.map (fun o -> o :: os) [Add; Multiply]
+let prepend_option os = List.map (fun o -> o :: os) !active_operators
 let rec possible_op_orders len = match len with
   | 0 -> [[]]
   | _ -> List.concat_map prepend_option (possible_op_orders (len-1))
@@ -21,8 +29,10 @@ let fixable_eq (test_val, eq) =
   let pops = possible_op_orders ((List.length eq) - 1) in 
   if List.exists (test_eq test_val eq) pops then Some test_val else None
 
+let solve eqs = List.filter_map fixable_eq eqs |> List.fold_left (+) 0
 
-(* Parsing *)
+
+(* Parse *)
 
 let read_lines file =
   let contents = In_channel.with_open_bin file In_channel.input_all in
@@ -35,6 +45,14 @@ let parse lines =
   |> List.map (String.split_on_char ':')
   |> List.map (fun l -> (int_of_string (List.hd l), parse_eq (List.nth l 1)))
 
-let part1 =
-  let eq_tests = parse (read_lines "input.txt") in
-  List.filter_map fixable_eq eq_tests |> List.fold_left (+) 0
+
+(* Execute *)
+
+let input = parse (read_lines "input.txt")
+let execute active_ops =
+  let _ = active_operators := active_ops in
+  solve input |> string_of_int |> print_endline
+
+let part1 = execute [Add; Multiply]
+let part2 = execute [Add; Multiply; Concat]
+  
